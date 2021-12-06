ca8ba92ebab5286966dc3892d1ea5066652ef308
Alister <alister.greg@gmail.com>
2021-12-06 19:23:13 +0000

    asd

--- upload-package.py
+++ upload-package.py
@@ -7,4 +7,16 @@ print('This is the internal script')
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 
-response = requests.get("www.dummyurl.com")     # To execute get request 
+arguments = sys.argv[1:]
+
+APIurl =  arguments[0];
+print ('url', str(APIurl))
+APItoken =  arguments[1];
+print ('token', str(APItoken))
+projectId =  arguments[2];
+print ('projId', str(projectId))
+artifactName =  arguments[3];
+print ('name', str(artifactName))
+#Get acsess token
+#Get all packages
+#response = requests.get("www.dummyurl.com")     # To execute get request 
