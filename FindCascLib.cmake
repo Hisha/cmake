89017a6c980ea8c71d0de936987f474b1606a7e4
Skarn <skarnproject@gmail.com>
2021-12-10 12:50:02 +0300

    search storm and casc on linux correctly
    search storm and casc on linux correctly

--- FindCascLib.cmake
+++ FindCascLib.cmake
@@ -18,9 +18,9 @@ endif()
 
 find_path (CASCLIB_INCLUDE_DIR CascLib.h CascPort.h)
 
-find_library (_casc_debug_lib NAMES CascLibDAD CascLibDAS CascLibDUD CascLibDUS PATHS ${CASCLIB_LIBRARY_DEBUG_DIR})
-find_library (_casc_release_lib NAMES CascLibRAD CascLibRAS CascLibRUD CascLibRUS PATHS ${CASCLIB_LIBRARY_RELEASE_DIR})
-find_library (_casc_any_lib NAMES Casc Casclib CascLib)
+find_library (_casc_debug_lib NAMES CascLibDAD CascLibDAS CascLibDUD CascLibDUS casc casclib CascLib PATHS ${CASCLIB_LIBRARY_DEBUG_DIR})
+find_library (_casc_release_lib NAMES CascLibRAD CascLibRAS CascLibRUD CascLibRUS  casc casclib CascLibPATHS ${CASCLIB_LIBRARY_RELEASE_DIR})
+find_library (_casc_any_lib NAMES casc casclib CascLib)
 
 set (CASC_LIBRARIES)
 if (_casc_debug_lib AND _casc_release_lib)
--- FindStormLib.cmake
+++ FindStormLib.cmake
@@ -18,8 +18,8 @@ endif()
 
 find_path (STORM_INCLUDE_DIR StormLib.h StormPort.h)
 
-find_library (_storm_debug_lib NAMES StormLibDAD StormLibDAS StormLibDUD StormLibDUS PATHS ${STORM_LIBRARY_DEBUG_DIR})
-find_library (_storm_release_lib NAMES StormLibRAD StormLibRAS StormLibRUD StormLibRUS PATHS ${STORM_LIBRARY_RELEASE_DIR})
+find_library (_storm_debug_lib NAMES StormLibDAD StormLibDAS StormLibDUD StormLibDUS storm stormlib StormLib PATHS ${STORM_LIBRARY_DEBUG_DIR})
+find_library (_storm_release_lib NAMES StormLibRAD StormLibRAS StormLibRUD StormLibRUS storm stormlib StormLib PATHS ${STORM_LIBRARY_RELEASE_DIR})
 find_library (_storm_any_lib NAMES storm stormlib StormLib)
 
 set (STORM_LIBRARIES)
