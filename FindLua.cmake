b1a398f7d35359b720bbe6c0b1e0b43c127887a7
Alister <alister.greg@gmail.com>
2021-12-13 18:03:35 +0000

    Update FindLua.cmake

--- FindLua.cmake
+++ FindLua.cmake
@@ -1,14 +1,13 @@
 # This file is part of Noggit3, licensed under GNU General Public License (version 3).
 
 # Dependency: lua
-FetchContent_Declare(
-    lua
+FetchContent_Declare(lua
   GIT_REPOSITORY https://gitlab.com/prophecy-rp/dependencies.git
   GIT_TAG        dep-lua
 )
 
 FetchContent_GetProperties(lua)
-IF(NOT lodepng_POPULATED)
+IF(NOT lua_POPULATED)
   MESSAGE(STATUS "Installing Lua...")
   FetchContent_Populate (lua)
   SET(LUA_INCLUDE_DIR "${lua_SOURCE_DIR}/includes")
@@ -16,13 +15,12 @@ IF(NOT lodepng_POPULATED)
   SET(LUA_LIBRARY_RELEASE_DIR "${lua_SOURCE_DIR}/lib/release/x64")
 ENDIF()
 
-find_library(_lua_debug_lib NAMES Lua PATHS ${LUA_LIBRARY_DEBUG_DIR})
-find_library(_lua_release_lib NAMES Lua PATHS ${LUA_LIBRARY_RELEASE_DIR})
+FIND_LIBRARY(_lua_debug_lib NAMES Lua PATHS ${LUA_LIBRARY_DEBUG_DIR})
+FIND_LIBRARY(_lua_release_lib NAMES Lua PATHS ${LUA_LIBRARY_RELEASE_DIR})
 
-set (LUA_LIBRARIES)
-list (APPEND LUA_LIBRARIES debug ${_lua_debug_lib} optimized ${_lua_release_lib})
-
+SET(LUA_LIBRARIES)
+LIST(APPEND LUA_LIBRARIES debug ${_lua_debug_lib} optimized ${_lua_release_lib})
 ADD_LIBRARY(Lua-Lua INTERFACE)
 ADD_LIBRARY(Lua::Lua ALIAS Lua-Lua)
-TARGET_LINK_LIBRARIES(Lua-Lua INTERFACE ${CASC_LIBRARIES})
+TARGET_LINK_LIBRARIES(Lua-Lua INTERFACE ${LUA_LIBRARIES})
 TARGET_INCLUDE_DIRECTORIES(Lua-Lua INTERFACE ${LUA_INCLUDE_DIR})
