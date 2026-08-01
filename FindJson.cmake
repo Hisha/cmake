21a79963f9e0b10b0aebcf8e23cadf2b939af0f3
Kevin Smith <vndinmchine@gmail.com>
2026-08-01 10:07:22 -0400

    Update FindJson.cmake

--- FindJson.cmake
+++ FindJson.cmake
@@ -1,16 +1,23 @@
 # This file is part of Noggit3, licensed under GNU General Public License (version 3).
 
-# Dependency: json.hpp
-# FetchContent_Declare (json
-#   GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent
-#   GIT_TAG v3.9.1
-# )
-FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz)
-FetchContent_MakeAvailable(json) # replaces FetchContent_PopulateFast
+# Dependency: nlohmann/json
+FetchContent_Declare(
+  json
+  GIT_REPOSITORY https://github.com/nlohmann/json.git
+  GIT_TAG v3.12.0
+  GIT_SHALLOW TRUE
+)
 
-# FetchContent_GetProperties (json)
-# IF(NOT json_POPULATED)
-#   MESSAGE(STATUS "Installing json.hpp...")
-#   FetchContent_PopulateFast(json)
-# ENDIF()
-# ADD_SUBDIRECTORY(${json_SOURCE_DIR} ${json_BINARY_DIR} EXCLUDE_FROM_ALL)
+FetchContent_GetProperties(json)
+
+IF(NOT json_POPULATED)
+  MESSAGE(STATUS "Installing json.hpp...")
+  FetchContent_PopulateFast(json)
+ENDIF()
+
+SET(JSON_BuildTests OFF CACHE INTERNAL "")
+ADD_SUBDIRECTORY(
+  ${json_SOURCE_DIR}
+  ${json_BINARY_DIR}
+  EXCLUDE_FROM_ALL
+)
