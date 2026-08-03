b8f2adc56cd25201e1af838cd7cd6f194df7cc98
Kevin Smith <vndinmchine@gmail.com>
2026-08-02 20:13:03 -0400

    Update FindSol2.cmake

--- FindSol2.cmake
+++ FindSol2.cmake
@@ -2,8 +2,8 @@
 
 # Dependency: sol2
 FetchContent_Declare (sol2
-        GIT_REPOSITORY https://github.com/ThePhD/sol2.git
-        GIT_TAG v3.3.0
+        GIT_REPOSITORY https://github.com/Hisha/sol2.git
+        GIT_TAG d557627c743b2ec53c3ea4cc84471b07caf8c7ef
         )
 FetchContent_MakeAvailable (sol2)
 # sol2::sol2 neither links lua nor sets include directories as system so will clobber us with
